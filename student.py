1d2380c5bbcbec6de106a339bad96592c168760d
OwenMcM <100361898+OwenMcM@users.noreply.github.com>
2022-03-29 09:09:55 -0400

    Update student.py

--- student.py
+++ student.py
@@ -1,6 +1,15 @@
 def main():
     #your code goes here
-    print("0")
+    num = int(input("CHOOSE THY NUMBA\n"))
+
+    if num % 5 == 0 and num % 3 == 0:
+        print ("FIZZBUZZ")
+    elif num % 3 == 0:
+        print("FIZZ")
+    elif num % 5 == 0:
+        print("BUZZ")
+    else:
+        print(num)
 
 if __name__ =='__main__':
     main()
